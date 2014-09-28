77a819951558f6c956bbea130a42ce51f3dd4295
xiazhou <zhyang@xiazhou-macbook.local>
2014-09-28 23:29:59 +0800

    Initial Commit

---
+++ interview_proj/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  interview_proj
+//
+//  Created by xiazhou on 9/28/14.
+//  Copyright (c) 2014 xiazhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
